31b279fa5b80d94c817f362f18b9ae35a9a85809
jozz024 <74272560+jozz024@users.noreply.github.com>
2025-03-23 23:56:43 -0500

    big big changes (code runs!)

--- sual.py
+++ sual.py
@@ -10,38 +10,34 @@ from discord.ext.commands import Bot
 
 load_dotenv()  # take environment variables from .env.
 
-discord_token = os.getenv("TOKEN")
+bot_token = os.getenv("TOKEN")
 
 BOT_PREFIX = "%" #command prefix
 
-intents = discord.Intents.default() #sets up the bots permissons (kinda) 
-intents.members = True
-intents.message_content = True
-intents.guilds = True
-intents.messages = True
+intents = discord.Intents.all() # changed this to all because its easier than that list
 bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents) # puts all the bot sthuff together so it can be ran
-tree = app_commands.CommandTree(bot)
 
 labid = 1328869164970020864
 
 challonge_obj = Challonge("7BXgk4lOBgdNAomWBfbUS4E7fgRZrDVElx82Wd7x")
 
 
+
+## changed this to on_ready because i realized what it was doing
 @bot.event
-async def on_message(message):
-    guildid = message.guild.id
-    await tree.sync(guild=discord.Object(id=guildid))
-    print("Ready!")
+async def on_ready():
+    await bot.tree.sync(guild=discord.Object(id=labid))
+    print(bot.user.name + " is online!") # prints to console when the bot is online
 
-
-@tree.command(
-    name="Set Report",
+## figured out that the tree was wrong because bot already defines a tree
+@bot.tree.command(
+    name="set_report", # cannot have spaces in command names
     description="Report da set",
-    guild=discord.Object(id=1328869164970020864)
+    guild=discord.Object(id=labid)
 )
-async def first_command(interaction):
+async def set_report(interaction: discord.Interaction, ):
     await interaction.response.send_message("Hello!")
- 
+
 
 
 bot.run(bot_token) #turns on the bot
